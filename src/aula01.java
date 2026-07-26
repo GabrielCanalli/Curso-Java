44032c345612ce322ce9d25c64699be2e4adf375
Gabriel Canalli Barbosa <gabrielcanalli9@gmail.com>
2026-07-26 19:16:04 -0300

    Adicionei cabeçalho documentado bilíngue na aula 01

--- src/aula01.java
+++ src/aula01.java
@@ -1,3 +1,24 @@
+/**
+ * ------------------------------------------------------------------------
+ * 🏫 Aula 01 - Introdução ao Java e Estrutura Básica
+ * ------------------------------------------------------------------------
+ * 📌 Resumo da Aula:
+ * - Apresentação da sintaxe básica da linguagem Java.
+ * - Explicação da estrutura de uma **class** e do **main method**.
+ * - Primeiro contato com saída de dados no console (**output**) usando
+ *   System.out.println().
+ * ------------------------------------------------------------------------
+ * 🇺🇸 Class 01 - Introduction to Java and Basic Structure
+ * ------------------------------------------------------------------------
+ * 📌 Class Summary:
+ * - Introduction to basic Java syntax.
+ * - Explanation of a **class** structure and the **main method**.
+ * - First contact with console **output** using System.out.println().
+ * ------------------------------------------------------------------------
+ * Autor / Author: Gabriel Canalli
+ * ------------------------------------------------------------------------
+ */
+
 public class aula01 {
 
     public static void main(String[] args) {
