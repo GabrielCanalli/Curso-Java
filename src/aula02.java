1ff75d03771a8153362465619458349091852afa
Gabriel Canalli Barbosa <gabrielcanalli9@gmail.com>
2026-07-31 21:14:53 -0300

    Desafio da aula02

--- src/aula02.java
+++ src/aula02.java
@@ -1,2 +1,18 @@
+import java.util.Scanner;
+
 public class aula02 {
+
+    public static void main(String[] args) {
+        var scanner = new Scanner(System.in);
+        System.out.println("Me diga uma temperatura em fahrenheit: ");
+        var tempf = scanner.nextInt();
+
+        final var ajuste = 32;
+        final var fator = 5/9.0;
+        var C = (tempf - ajuste) * fator;
+
+        System.out.println("A formula de conversão de fahrenheit para celsius é (°F - 32) x 5/9 = °C ");
+        System.out.printf("De acordo com a formula os %s fahrenheit são %.2f celsius.\n", tempf, C);
+
+    }
 }
